1d8f4cfb8703b28e77b278b6a55f49043f9df671
Han Jihye <haneh0714@gmail.com>
2022-07-27 16:37:34 +0900

    예제 프로젝트 추가

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 - proxy 전체가 component scan 되지 않도록 함
 public class ProxyApplication {
 
 	public static void main(String[] args) {
