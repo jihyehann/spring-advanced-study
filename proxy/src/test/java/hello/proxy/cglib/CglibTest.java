cf25c30bf1f9fe1365a9c1c96f51d542f5836bd4
Han Jihye <haneh0714@gmail.com>
2022-08-05 16:09:01 +0900

    CGLIB

---
+++ proxy/src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class); // 구체 클래스를 상속받아 프록시를 생성한다.
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
---
+++ proxy/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,30 @@
+package hello.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodProxy.invoke(target, args); // method.invoke()를 사용해도 되지만, methodProxy를 사용하는 것이 성능 상 더 권장된다.
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
