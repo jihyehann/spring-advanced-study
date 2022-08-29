9b525300e5d7cf1870b044ad6348d24479f72d5c
Han Jihye <haneh0714@gmail.com>
2022-08-29 13:22:47 +0900

    스프링 AOP 포인트컷 - 예제 생성

---
+++ aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,30 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException{
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class); // hello 메서드, 파라미터 String 타입
+    }
+
+    @Test
+    void printMethod() {
+        // public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        log.info("helloMethod={}", helloMethod);
+    }
+
+
+
+}
